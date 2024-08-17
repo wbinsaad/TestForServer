238aa11d3c007f6b675de1a3e74c300b75794cba
Waleed BinSaad <wbinsaad@stu.kau.edu.sa>
2024-08-17 21:04:01 +0300

    Add TextController

---
+++ Controllers/TextController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestForServer.Models;
+
+namespace TestForServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TextController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Index(TextDto req)
+        {
+            return Ok(new
+            {
+                req,
+                nn = Guid.NewGuid().ToString()
+            });
+        }
+    }
+}
