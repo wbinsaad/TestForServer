1b5303bf470f9407b2566e9fdfefe769a17f5594
Waleed BinSaad <wbinsaad@stu.kau.edu.sa>
2024-08-17 19:07:59 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestForServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
