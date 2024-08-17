0936fc9a4c9da616f9337f6c59bcfe12b0f51f69
Waleed BinSaad <wbinsaad@stu.kau.edu.sa>
2024-08-17 22:03:30 +0300

    Add TextController
    - Add TextController
    - Get, Post, Delete

--- Controllers/TextController.cs
+++ Controllers/TextController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestForServer.Database;
 using TestForServer.Models;
+using TestForServer.ModelViews;
 
 namespace TestForServer.Controllers
 {
@@ -8,14 +12,51 @@ namespace TestForServer.Controllers
     [ApiController]
     public class TextController : ControllerBase
     {
-        [HttpPost]
-        public IActionResult Index(TextDto req)
+        private readonly AppDbContext _appDbContext;
+        private readonly IMapper _mapper;
+
+        public TextController(AppDbContext appDbContext, IMapper mapper)
         {
-            return Ok(new
-            {
-                req,
-                nn = Guid.NewGuid().ToString()
-            });
+            _appDbContext = appDbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<OkObjectResult> Get()
+        {
+            var query = await _appDbContext
+                                       .Texts
+                                       .AsNoTracking()
+                                       .ToListAsync();
+
+            return Ok(query);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(TextDto req)
+        {
+            var reqMapped = _mapper.Map<Text>(req);
+            if (reqMapped is null) return BadRequest("something went wrong");
+            
+            reqMapped.IPAddress = HttpContext.Request.Host.Host;
+
+            await _appDbContext.AddAsync(reqMapped);
+            await _appDbContext.SaveChangesAsync();
+
+            return Ok(reqMapped);
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> Delete(Guid? Id)
+        {
+            if(Id is null) return NotFound();
+
+            var query = await _appDbContext.Texts.Where(x => x.Id == Id).FirstOrDefaultAsync();
+            if (query is null) return NotFound();
+
+            _appDbContext.Remove(query);
+            await _appDbContext.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
